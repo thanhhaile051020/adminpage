0eb834899f755297e1e23f792ee2782103013e62
thanhhaile0510@gmail.com <thanhhaile0510@gmail.com>
2021-12-25 01:39:59 +0700

    cmt

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -55,7 +55,7 @@ function Dashboard() {
       { fromTime: moment().subtract(1, "days"), toTime: moment() },
       getConfig()
     );
-    setCurrentDayActivity(result.data.countMax);
+    setCurrentDayActivity(result.data.count);
   };
   return (
     <>
@@ -89,7 +89,7 @@ function Dashboard() {
           </Col>
         </Row>
         <Row>
-          <Col md="8">
+          <Col md="12">
             <Card>
               <Card.Header>
                 <div
@@ -156,39 +156,7 @@ function Dashboard() {
               </Card.Footer>
             </Card>
           </Col>
-          <Col md="4">
-            <Card>
-              <Card.Header>
-                <Card.Title as="h4">Email Statistics</Card.Title>
-                <p className="card-category">Last Campaign Performance</p>
-              </Card.Header>
-              <Card.Body>
-                <div
-                  className="ct-chart ct-perfect-fourth"
-                  id="chartPreferences"
-                >
-                  <ChartistGraph
-                    data={{
-                      labels: ["40%", "20%", "40%"],
-                      series: [40, 20, 40],
-                    }}
-                    type="Pie"
-                  />
-                </div>
-                <div className="legend">
-                  <i className="fas fa-circle text-info"></i>
-                  Open <i className="fas fa-circle text-danger"></i>
-                  Bounce <i className="fas fa-circle text-warning"></i>
-                  Unsubscribe
-                </div>
-                <hr></hr>
-                <div className="stats">
-                  <i className="far fa-clock"></i>
-                  Campaign sent 2 days ago
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
+         
         </Row>
       </Container>
     </>
